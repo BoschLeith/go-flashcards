9c3a6bcb25c510050ad70d2732dfa18c8c8f110d
BoschLeith <bosch.leith@gmail.com>
2025-02-28 12:17:36 -0600

    Implement HTTP server with home route and logging

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
 
 func main() {
-	fmt.Println("hello world")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("Server started on localhost:8080")
+	
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
 }
